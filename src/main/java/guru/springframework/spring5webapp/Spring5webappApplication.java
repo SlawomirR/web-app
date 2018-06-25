bf36896b722c3436e73374d4dbeb9ba52aa4bca3
SlawomirR <no.for.spam@wp.pl>
2018-06-25 21:24:03 +0200

    Added feature to print in console registered repository's beans

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -2,11 +2,25 @@ package guru.springframework.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Spring5webappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5webappApplication.class, args);
+/*		SpringApplication.run(Spring5webappApplication.class, args);
+	}
+*/
+	ApplicationContext ctx = SpringApplication.run(Spring5webappApplication.class, args);
+	displayRepositoryBeans(ctx);
+}
+
+	public static void displayRepositoryBeans(ApplicationContext ctx) {
+		Arrays.asList(ctx.getBeanDefinitionNames())
+				.stream()
+				.filter(line -> line.contains("Repository"))
+				.forEach(repo -> System.out.println("Repo: " + repo));
 	}
 }
